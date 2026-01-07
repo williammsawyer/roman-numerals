f44ab42634132cc03bbb123b523bbf53a4181172
William Sawyer <2047323+williammsawyer@users.noreply.github.com>
2026-01-07 12:16:01 -0700

    Create c-sharp.cs

---
+++ c-sharp.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+public class RomanConverter {
+
+    /*
+      15 mins time box.
+      Please think out loud so we can get a sense of your thought process.
+      Ask questions when you are stuck.
+      Finishing it is not a requirement.
+      Syntax is not important.
+    */
+
+    // C# 9.0+ syntax allows 'new()' if type is inferred, 
+    // but explicit Dictionary is safer for interviews.
+    private static readonly Dictionary<char, int> RomanMap = new Dictionary<char, int>
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    /* THE LOGIC:
+     *
+     * GENERAL RULE (Addition):
+     * - Usually, you simply sum up the values of the symbols.
+     * - Example: VI -> 5 + 1 = 6
+     * - Example: VII -> 5 + 1 + 1 = 7
+     *
+     * THE EXCEPTION (Subtraction):
+     * - If a smaller value appears *before* a larger value, it is subtracted from the total.
+     * - Example: IV -> -1 + 5 = 4 
+         Because I(1) is smaller than V(5), we subtract.  
+     * - Example: IX -> -1 + 10 = 9 
+         Because I(1) is smaller than X(10), we subtract.  
+     *
+     * COMPLEX EXAMPLE:
+     * XIV -> 10 - 1 + 5 = 14
+     */
+
+    public int RomanToInt(string s) {
+        // TODO: Your code here
+        return 0;
+    }
+
+    public static void Main(string[] args) {
+        Console.WriteLine(new Solution().RomanToInt("IV"));
+    }
+}
