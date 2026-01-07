f8020386287c7d9100b42529bc06c53a7d1f87c5
William Sawyer <2047323+williammsawyer@users.noreply.github.com>
2026-01-07 12:25:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const romanMap = {
  * GENERAL RULE (Addition):
  * - Usually, you simply sum up the values of the symbols.
  * - Example: VI -> 5 + 1 = 6
- * - Example: VII -> 5 + 1 + 1) = 7
+ * - Example: VII -> 5 + 1 + 1 = 7
  *
  * THE EXCEPTION (Subtraction):
  * - If a smaller value appears *before* a larger value, it is subtracted from the total.
